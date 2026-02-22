12c472de236118c5018d01ac705a9fa32060ae7b
hailemaryam <hailemaryammecca@gmail.com>
2026-02-22 19:49:26 +0300

    added to applist

--- post_office/hooks.py
+++ post_office/hooks.py
@@ -11,15 +11,14 @@ app_license = "mit"
 # required_apps = []
 
 # Each item in the list will be shown as an app in the apps page
-# add_to_apps_screen = [
-# 	{
-# 		"name": "post_office",
-# 		"logo": "/assets/post_office/logo.png",
-# 		"title": "Post Office",
-# 		"route": "/post_office",
-# 		"has_permission": "post_office.api.permission.has_app_permission"
-# 	}
-# ]
+add_to_apps_screen = [
+	{
+		"name": "post_office",
+		"logo": "/assets/post_office/logo.svg",
+		"title": "Post Office",
+		"route": "/app/post-office-dashboard",
+	}
+]
 
 # Includes in <head>
 # ------------------
